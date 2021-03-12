10e7fb89caa4665dcc27e86e432704516acb3e96
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-03-13 02:11:40 +0530

    bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ class App extends Component {
     requestUser: "",
     amount: "",
     acceptData: {},
-    confirmStatus: true,
+    confirmStatus: false,
     confirm: "",
   };
   messages = [];
